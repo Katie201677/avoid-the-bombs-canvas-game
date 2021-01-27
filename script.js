ae78c2315b18efa2642e7a52c984bab7ef54ff2e
Katie201677 <63996368+Katie201677@users.noreply.github.com>
2021-01-27 09:51:18 +0000

    Canvas rendering successfully

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-const canvas = document.querySelector('#canvas');
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+
+// draw rectangle to test
+// ctx.beginPath();
+// ctx.rect(30, 30, 50, 50);
+// ctx.fillStyle = "blue";
+// ctx.fill();
+// ctx.closePath();
